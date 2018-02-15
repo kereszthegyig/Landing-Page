660bd9565951cc26c6a4e08108ed7c703687a5d5
kereszthegyig <kereszthegyig@gmail.com>
2018-02-15 09:10:07 +0000

    Landing page v1

---
+++ app.js
@@ -0,0 +1,21 @@
+
+var express = require("express");
+var app = express();
+
+
+app.set('view engine','ejs');
+app.use(express.static(__dirname + "/public"));
+
+
+//ROUTES
+
+//INDEX ROUTE
+
+app.get('/', function(req,res) {
+    res.render('index');
+})
+
+
+app.listen(process.env.PORT,process.env.IP,function(){
+    console.log('Server has started!');
+})
